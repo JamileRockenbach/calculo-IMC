126de72485a1348e80a9a6fd1cebcc180d0254c7
Jamile <jamilerockenbach22@gmail.com>
2025-09-03 21:47:46 -0300

    Variaveis feitas.

--- AtividadeImc.java
+++ AtividadeImc.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class AtividadeImc {
     public static void main(String[] args) {
+        Scanner lerTeclado = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = lerTeclado.nextLine();
+        lerTeclado.nextLine();
+
+        System.out.println("Digite seu gênero (F/M/N): ");
+        char genero = lerTeclado.nextLine().charAt(0);
+        lerTeclado.nextLine();
+
+        System.out.println("Digite sua altura: ");
+        double altura = lerTeclado.nextDouble();
+        lerTeclado.nextLine();
+
+        System.out.println("Digite seu peso em quilos: ");
+        double peso = lerTeclado.nextDouble();
+        lerTeclado.nextLine();
         
+        double imc = peso / (altura * altura);
+        String classificacao = "";
+
+        //começa o switch
     }
 }
